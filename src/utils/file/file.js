d4926a8e0ad8d06357cd8d55e545a3a5a12f28e9
ycao <yuancaocc@163.com>
2021-12-29 22:03:31 +0800

    webpack

---
+++ src/utils/file/file.js
@@ -0,0 +1,9 @@
+/**
+ * 得到文件夹里所有文件数量
+ * Created by ycao on 2021-12-29 20:16:30
+ */
+// export function getDirectoryFilesNum(directory){
+//     const files = require.context(directory,true,/\.jpeg$/)
+//     //keys: 返回匹配成功模块的名字组成的数组
+//     return files.keys().length;
+// }
