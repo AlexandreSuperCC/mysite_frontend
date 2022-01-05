35051afd1cca18c072e9f37f79d14d86fe197506
yuancaocc <cy5509902415>
2022-01-05 09:50:11 +0800

    20211230

--- src/components/element-pp/ContactMe.vue
+++ src/components/element-pp/ContactMe.vue
@@ -3,19 +3,28 @@
     <div class="cm-left"></div>
     <div class="cm-middle">
       <el-steps :active="curActive">
-        <el-step title="phone number" description="06 68 25 20 48" @click="changeActive(0)">
+        <el-step title="phone number" @click="changeActive(0)">
           <template v-slot:icon>
             <img class="smartphone-icon" src="~assets/index/images/contact/smartphone.png" alt="phone">
           </template>
+          <template v-slot:description>
+            <a class="my-telephone" href="tel:0668252048">+(33) 06 68 25 20 48</a>
+          </template>
         </el-step>
-        <el-step title="e-mail" description="yuancaocc@gmail.com" @click="changeActive(1)">
+        <el-step title="e-mail" @click="changeActive(1)">
           <template v-slot:icon>
             <img class="email-icon" src="~assets/index/images/contact/email.png" alt="email">
           </template>
+          <template v-slot:description>
+            <a class="my-email" target="_blank" href="mailto:yuan.cao@utbm.fr">yuan.cao@utbm.fr</a>
+          </template>
         </el-step>
-        <el-step title="linkedin" description="linkedin.com/in/yuan-cao-75b112208" @click="changeActive(2)">
+        <el-step title="linkedin" @click="changeActive(2)">
           <template v-slot:icon>
             <img class="linkedin-icon" src="~assets/index/images/contact/linkedin.png" alt="linkedin">
+          </template>
+          <template v-slot:description>
+            <a class="my-linkedin" target="_blank" href="https://www.linkedin.com/in/yuan-cao-75b112208">linkedin.com/in/yuan-cao-75b112208</a>
           </template>
         </el-step>
         <el-step title="location" description="Belfort, France" @click="changeActive(3)">
@@ -71,4 +80,14 @@ export default {
   background-color: #e8e9af;
 ;
 }
+a{
+  color: #0022ff;
+}
+/*a:link{*/
+/*  color: #0022ff*/
+
+/*}*/
+/*a:visited{*/
+/*  color: #5469f1*/
+/*}*/
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports={
+    //网站图标设置
+    pwa: {
+        iconPaths: {
+            favicon32: 'favicon.ico',
+            favicon16: 'favicon.ico',
+            appleTouchIcon: 'favicon.ico',
+            maskIcon: 'favicon.ico',
+            msTileImage: 'favicon.ico'
+        }
+    },
     configureWebpack:{
         resolve:{
             alias:{
