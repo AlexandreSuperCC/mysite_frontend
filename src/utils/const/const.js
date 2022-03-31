3ecf831293e017d81180a0f7045bbe6cec552952
Yuan Cao <yuancaocc@gmail.com>
2022-03-31 12:23:16 +0200

    Update const.js

--- src/utils/const/const.js
+++ src/utils/const/const.js
@@ -12,5 +12,5 @@ export const myPhotoPath = httpOrHttps+'assets.cklovery.life/myPhoto'
 //assets 七牛云文件链接地址 【塞到goolemap源码里的获取当前位置的图标】
 export const gMapCurPosPath = httpOrHttps+'aassets.cklovery.life/gmap/sniperLocate.png'
 //google map api key
-export const googleMapPrivateKey = 'AIzaSyDXsk3ofO4ni-AHd1m5ut1AVwVvyJA4NNA'
+export const googleMapPrivateKey = ''
 
